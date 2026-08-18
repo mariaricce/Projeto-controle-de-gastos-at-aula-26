6a0fbdd9286af5287d38adc2a1b074ad7f5807c0
mariaricce <ricce.maria@escola.pr.gov.br>
2026-08-18 15:03:20 -0300

    Atualizando projeto controle de gastos

---
+++ firebase-init.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB2JhZOZoCpJWyVb46Mr_L7WDBmcLcg-Ac",
+    authDomain: "controle-gastos-964b6.firebaseapp.com",
+    projectId: "controle-gastos-964b6",
+    storageBucket: "controle-gastos-964b6.firebasestorage.app",
+    messagingSenderId: "794617114798",
+    appId: "1:794617114798:web:b0f3fc4ca5d96dd1303d3f",
+    measurementId: "G-6SNWT4XQ5H"
+};
+
+firebase.initializeApp(firebaseConfig);
